dc14dd522565c6a6990682ca3dde7a084bc3b44d
Karan Shrestha <karanshrestha96@gmail.com>
2020-10-04 23:59:33 -0400

    Setup Firebase SDK snippet

---
+++ whatsapp-mern/src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Chat.css"
+
+function MyComponent() {
+  return (
+    <div className="chat">
+    <h1>I'm chat</h1>
+    </div>
+  )
+}
+
+export default MyComponent
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAW1CSTxgcfcd_KQhY2i2JVnzfk8MfuXE",
+  authDomain: "whatsapp-mern-553b5.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-553b5.firebaseio.com",
+  projectId: "whatsapp-mern-553b5",
+  storageBucket: "whatsapp-mern-553b5.appspot.com",
+  messagingSenderId: "354939019276",
+  appId: "1:354939019276:web:6bb6c2979460fb46bd0e29"
+};
