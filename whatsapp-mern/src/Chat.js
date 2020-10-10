1cdeb937a29664702fc1ad35044a5df170cef36e
Karan Shrestha <karanshrestha96@gmail.com>
2020-10-10 18:07:29 -0400

    Integrated the sidechat bar and header of the main chat box

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import "./Chat.css"
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 
-function MyComponent() {
+
+function Chat() {
   return (
     <div className="chat">
-    <h1>I'm chat</h1>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p>
+          <span className="chat__name">Karan</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+      </div>
     </div>
   )
 }
 
-export default MyComponent
+export default Chat;
